6b64dd26a823f280cd92b361654f38a4a937872a
Jozef Szczygiel <j.szczygiel@corecomtechacademy.co.uk>
2024-08-16 11:36:36 +0100

    Layout Comments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,36 @@ public class App {
 
     public static void main
     // Dwayne = Rock, Brendan = Paper, Johnny = Scissors
-    
+
+
+    // User to enter move
+
+    //Valid move entered check
+
+    //Randomizer element for opponents move
+
+    // Win, Lose, Draw?
+
+    // If Player chose rock,
+    //Opponent Rock - Draw
+    //Opponent Paper - Player Lose
+    //Opponent Scissors - Player Win
+
+    // If Player chose Paper,
+    //Opponent Rock - Player Win
+    //Opponent Paper - Draw
+    //Opponent Scissors - Player Lose
+
+    // If Player chose Scissors,
+    //Opponent Rock - Player Lose
+    //Opponent Paper - Player win
+    //Opponent Scissors - Draw
+
+
+    //Play again? loop?
+
+    //quit?
+
+
 }
 
